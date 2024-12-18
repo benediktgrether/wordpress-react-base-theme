4c0fc49016974002ab502c76cf3916c7f2b21c19
Benedikt <info@benediktgrether.de>
2024-12-18 22:10:43 +0100

    Add docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://docs.everyday.agency/',
+
+    integrations: [
+        starlight({
+            title: 'everyday.agency Documentation',
+            social: {
+                github: 'https://github.com/withastro/starlight',
+            },
+            sidebar: [
+                {
+                    label: 'Getting Started',
+                    autogenerate: { directory: 'getting-started' },
+                },
+                {
+                    label: 'Configuration',
+                    autogenerate: { directory: 'configuration' },
+                },
+                {
+                    label: 'Layout',
+                    autogenerate: { directory: 'layout' },
+                },
+                {
+                    label: 'Utilities',
+                    autogenerate: { directory: 'utilities' },
+                },
+                {
+                    label: 'Components',
+                    autogenerate: { directory: 'components' },
+                },
+                {
+                    label: 'WordPress',
+                    autogenerate: { directory: 'wordpress' },
+                },
+            ],
+        }),
+    ],
+});
