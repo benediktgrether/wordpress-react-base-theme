bf372555b023d7a4ffae85d91108753073d1180c
Benedikt Grether <info@benediktgrether.de>
2024-12-19 17:40:18 +0100

    Add container and grid to layout, add not-found and main layout elements

---
+++ blocks/layout/main/main.jsx
@@ -0,0 +1,32 @@
+//https://wordpress.github.io/gutenberg/?path=/story/icons-icon--library
+import { registerBlockType } from '@wordpress/blocks';
+import { layout } from '@wordpress/icons';
+registerBlockType('basetheme/main', {
+    title: 'Main',
+    icon: layout,
+    category: 'layout',
+    description: 'A Description',
+    keywords: '[]',
+    supports: {},
+    attributes: {},
+
+    edit: EditComponent,
+    save: SaveComponent,
+});
+
+function EditComponent(props) {
+    const { attributes, setAttributes } = props;
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+function SaveComponent() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
