4d78088be6fb49188504b4b3ebbcf3b7e4f8b0be
Benedikt Grether <info@benediktgrether.de>
2025-08-06 10:11:51 +0200

    Add warning class to reference

--- build/reference.jsx.asset.php
+++ build/reference.jsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-primitives'), 'version' => '5b1a38a5347d32d5cae6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-primitives'), 'version' => '94425949a6407c49b9c2');
